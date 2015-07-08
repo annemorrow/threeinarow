246354b348c6234a71ed8f01a75b99ac2c1f348f
Anne Morrow <anne.moore2008@gmail.com>
2015-07-08 16:20:18 -0600

    real buttons now

--- threeinarow.js
+++ threeinarow.js
@@ -2,7 +2,7 @@ function Fraction(numerator, denominator) {
   this.numerator = numerator;
   this.denominator = denominator;
   this.htmlString = function() {
-    var str = '<button class="fraction"><div class="numerator">' + numerator + '</div>'
+    var str = '<button class="fraction" onclick="selectFraction(this)"><div class="numerator">' + numerator + '</div>'
     str += '<div class="denominator">' + denominator + '</div></button>';
     return str;
   }
@@ -40,11 +40,11 @@ var dotsArray = {
     var dot = this.playedList[dotIndex];
     var margin;
     if (dotIndex == 0) {
-      // the 5 is half the width of the dot
-      margin = Math.floor(1000 * dot.value) -5 ;
+      // the 10 the width of the dot
+      margin = Math.floor(1000 * dot.value) -10 ;
     } else {
       margin = dot.value - this.playedList[dotIndex - 1].value;
-      margin = Math.floor(1000 * margin) -5;
+      margin = Math.floor(1000 * margin) -10;
     }
     var str = '<div class="dot ' + dot.player + '" style="margin-left:' + margin +'px"></div>';
     return str;
@@ -58,4 +58,56 @@ var dotsArray = {
       console.log(el.innerHTML);
     }
   }
+}
+
+// fractions and decimals
+var fractionCatalogue = [
+  new Fraction(1, 2),
+  new Fraction(1, 4),
+  new Fraction(1, 3),
+  new Fraction(1, 5),
+  new Fraction(1, 10),
+  new Fraction(3, 4),
+  new Fraction(9, 10),
+  new Fraction(7, 8),
+  new Fraction(2, 3),
+  new Fraction(4, 5),
+  new Fraction(3, 5),
+  new Fraction(1, 8),
+  new Fraction(7, 10),
+  new Fraction(3, 5),
+  new Fraction(3, 8),
+  new Fraction(4, 9),
+  new Fraction(1, 20),
+  new Fraction(5, 6),
+  new Fraction(1, 6),
+  new Fraction(3, 10),
+]
+
+for (var i = 0; i < 20; i++) {
+  document.getElementById("fractions").innerHTML += fractionCatalogue[i].htmlString();
+}
+
+var player = "player1";
+
+function switchPlayer() {
+  console.log("switchPlayer called");
+  var heading = document.getElementsByTagName("h3")[0];
+  if (player == "player1") {
+    player = "player2";
+    heading.innerHTML = "Player 2";
+  } else if (player == "player2") {
+    player = "player1";
+    heading.innerHTML = "Player 1";
+  }
+}
+
+function selectFraction(button) {
+  var num = Number(button.getElementsByClassName("numerator")[0].innerHTML);
+  var denom = Number(button.getElementsByClassName("denominator")[0].innerHTML);
+  var dot = new Dot(player, num/denom);
+  dotsArray.addDot(dot);
+  dotsArray.placeDots();
+  button.disabled = true;
+  switchPlayer();
 }
